65bed3b9297b18a4802a95e3334c40c581427e12
DHoore Billy <billy.dhoore@immalle.eu>
2017-09-28 09:45:41 +0200

    Het programma returnt 1 getal nl. het aantal landen aanwezig in de database

--- WorldDbQuerier/Program.cs
+++ WorldDbQuerier/Program.cs
@@ -5,7 +5,7 @@ namespace WorldDbQuerier
     class Program
     {
 
-        static string version = "0.1";
+        static string version = "0.2";
 
         static void Main(string[] args)
         {
@@ -19,19 +19,14 @@ namespace WorldDbQuerier
                         Console.WriteLine("Onbekend argument");
                         break;
                 }
+
             MySqlConnection comm = new MySqlConnection();
-
-
-            comm.ConnectionString = "Server = 192.168.56.101;Port = 3306; Database = Concertzalen1;Uid = root;Pwd = imma; ";
-
             MySqlCommand cmd = new MySqlCommand();
+            comm.ConnectionString = "Server = 192.168.56.101;Port = 3306; Database = world;Uid = root;Pwd = imma; ";
             cmd.Connection = comm;
-            cmd.CommandText = "SELECT count(*) FROM tblConcertzalen";
-
             comm.Open();
-
-
-            Console.WriteLine("Aantal concertzalen {0}", cmd.ExecuteScalar());
+            cmd.CommandText = "SELECT count(*) FROM Country";
+            Console.WriteLine("Aantal landen {0}", cmd.ExecuteScalar());
 
         }
     }
