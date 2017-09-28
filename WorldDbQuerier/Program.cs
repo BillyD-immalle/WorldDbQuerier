ce173f1b93cf7eba5b9839ddfbef2c5b34395d4a
DHoore Billy <billy.dhoore@immalle.eu>
2017-09-28 10:05:45 +0200

    Begonnen met lijst van de landen afdrukken

--- WorldDbQuerier/Program.cs
+++ WorldDbQuerier/Program.cs
@@ -15,18 +15,19 @@ namespace WorldDbQuerier
                     case "-v":
                         Console.WriteLine("v" + version);
                         break;
+                    case "aantal":
+                        AantalLanden();
+                        break;
+                    case "lijst":
+                        LijstLanden();
+                        break;
                     default:
                         Console.WriteLine("Onbekend argument");
                         break;
-                }
-            else
-
-            Database();
-            
-
+                }                
+           
         }
-
-        public static void Database()
+        public static void AantalLanden()
         {
             MySqlConnection comm = new MySqlConnection();
             MySqlCommand cmd = new MySqlCommand();
@@ -36,6 +37,18 @@ namespace WorldDbQuerier
             cmd.CommandText = "SELECT count(*) FROM Country";
             Console.WriteLine("Aantal landen {0}", cmd.ExecuteScalar());
         }
+        public static void LijstLanden()
+        {
+            MySqlConnection comm = new MySqlConnection();
+            MySqlCommand cmd = new MySqlCommand();
+            comm.ConnectionString = "Server = 192.168.56.101;Port = 3306; Database = world;Uid = root;Pwd = imma; ";
+            cmd.Connection = comm;
+            comm.Open();
+            cmd.CommandText = "SELECT * FROM Country";
+            cmd.ExecuteReader();
+            }
+        }
 
-    }
+
+
 }
